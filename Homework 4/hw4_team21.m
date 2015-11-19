488d49d3164b7d67d2f41a0cd0a8a7e4d57a5e4b
Naina Prasad <prasad.naina@gmail.com>
2015-11-19 01:40:53 -0500

    MATLAB NOT WORKING - UPDATED V

--- Homework 4/hw4_team21.m
+++ Homework 4/hw4_team21.m
@@ -1,36 +1,32 @@
-function hw4_team21(textfile)
+function hw4_team21(serPort);
     % takes in textfile that was output by
     % dijkstra's algorithm in python
     % "shortestpath.txt"
-    
+
     % opens the file to read
-    fid = fopen(textfile);
-    
+    fid = fopen('shortestPath.txt');
+    tmp = fscanf(fid,'%f');
+    fclose(fid);
     % reads in the start point
-    tline = fget1(fid);
-    x = float(point(0));
-    y = float(point(1));
+    x = tmp(1);
+    y = tmp(2);
     
     % while there are nodes to be travelled to
-    while ischar(tline)
+    for i=3:2:length(tmp)
         % read the point
-        point = strsplit(tline);
+        %temp = strsplit(tmp)
         % change the offset
-        x = float(point(0)) - x;
-        y = float(point(1)) - y;
+        x = tmp(i) - x;
+        y = tmp(i+1) - y;
         % calculate angle to be turned
         theta = atan(y/x);
         % calculate distance to be travelled
         d = sqrt(y^2 + x^2);
         % turn the angle
-        turnAngle(90-theta);
+        turnAngle(serPort,0.1,90-theta);
         % travel the distance
-        travelDist(d);
+        travelDist(serPort,0.4,d);
         % turn back to original angle
-        turnAngle(-(90-theta))
-        % read next line
-        tline = fget1(fid);
+        turnAngle(serPort,0.1,-(90-theta));
     end
-    % close the file
-    fclose(fid);
 end
